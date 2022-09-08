156eb2685d67cca71007be1b6dba1154616bf22c
Seemingly <2781343172@qq.com>
2022-09-09 00:20:52 +0800

    定时器组声明
    1.单个定时器修改(增加状态标记位)
    2.定时器组声明

--- code/cpp/timer.cpp
+++ code/cpp/timer.cpp
@@ -1,15 +1,22 @@
 
 #include"timer.h"
 
-timer::timer(int time_ms,void* data,void* (*call_back)(void*))
+timer_one::timer_one(int time_ms,void* data,void* (*call_back)(void*))
 {
     this->start_time=clock();
     this->end_time=this->start_time+time_ms;
+    this->state_flag=true;
     this->data=data;
     this->call_back=call_back;
 }
 
-void* timer::run_call_back()
+void timer_one::set_state_flag(bool flag)
+{
+    this->state_flag=flag;
+    return ;
+}
+
+void* timer_one::run_call_back()
 {
     return this->call_back(this->data);
 }
--- code/cpp/timer.h
+++ code/cpp/timer.h
@@ -9,17 +9,31 @@
 #include<ctime>
 
 //单个定时器定义
-class timer
+class timer_one
 {
     private:
         time_t start_time;                          //起始时间
         time_t end_time;                            //结束时间
+        bool state_flag;                            //定时器状态  有效true 无效false(可能删除)
         void* data;                                 //回调函数所需要的数据
         void* (*call_back)(void*);                  //回调函数
     public:
-        timer(int time_ms,void* data,void* (*call_back)(void*));     //初始化函数  参数为 定时时长+回调函数
+        timer_one(int time_ms,void* data,void* (*call_back)(void*));     //初始化函数  参数为 定时时长+回调函数
+        void set_state_flag(bool flag);             //设置定时器状态
         void* run_call_back();                      //执行回调函数 
 };
+
+//定时器组  外部调用
+class timers
+{
+    private:
+        std::priority_queue <time_t,std::vector<timer_one>,std::less<timer_one> >q;     //优先队列
+    public:
+        timers();                                   
+        timer_one* add_timers(timer_one timer);                                         //添加定时器
+        bool delete_timers();                                                           //删除定时器      
+        bool update_timers();                                                           //更新定时器
+}
 
 
 
