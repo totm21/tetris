e82765ea1dee2ed624d7d29e4ad1dcc9bc5c322b
Seemingly <2781343172@qq.com>
2022-10-26 20:16:10 +0800

    表格

--- code/cpp/configure.h
+++ code/cpp/configure.h
@@ -8,19 +8,21 @@
 
 #include"expands/json/json.h"
 
+//单条数据
 class mb_node
 {
     public:
         int id;
-        void* data;     
+        std::vector<void*> data;
 };
 
+//单个表数据
 class mb_data
 {
     public:
+        std::string name;
         std::vector<char> type;      //表头类型
         std::vector<mb_node> data;
-
 
 };
 
